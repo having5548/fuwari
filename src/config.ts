ba2a6cf293aa438bdc536e1a8bb0bb702c2351df
having5548 <125118486+having5548@users.noreply.github.com>
2026-05-29 22:18:29 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -119,7 +119,7 @@ export const umamiConfig: UmamiConfig = {
 // 防盗链/域名保护配置
 export const antiLeechConfig: AntiLeechConfig = {
 	enable: true,
-	officialSites: [{ url: "me.223910.xyz", name: "主站" }],
+	officialSites: [{ url: "https://me.223910.xyz", name: "主站" }],
 	debug: false,
 	warningTitle: "⚠️ 域名安全警告",
 	warningMessage:
